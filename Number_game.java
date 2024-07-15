8dedd70b60188c4d6fdfb8fdc357049f4255aba1
NithishS2003 <nithishkumarnithish112@gmail.com>
2024-07-15 10:30:18 +0530

    Create Number_game.java

---
+++ Number_game.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int score = 0;
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int numberToGuess = random.nextInt(100) + 1;
+            int attempts = 0;
+            int maxAttempts = 10;
+            boolean hasGuessedCorrectly = false;
+
+            System.out.println("I have generated a number between 1 and 100. Can you guess it?");
+            System.out.println("You have " + maxAttempts + " attempts.");
+
+            while (attempts < maxAttempts && !hasGuessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == numberToGuess) {
+                    System.out.println("Congratulations! You guessed the correct number.");
+                    hasGuessedCorrectly = true;
+                    score++;
+                } else if (userGuess < numberToGuess) {
+                    System.out.println("Your guess is too low.");
+                } else {
+                    System.out.println("Your guess is too high.");
+                }
+            }
+
+            if (!hasGuessedCorrectly) {
+                System.out.println("Sorry, you've used all your attempts. The correct number was: " + numberToGuess);
+            }
+
+            System.out.println("Your current score is: " + score);
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String userResponse = scanner.next();
+            playAgain = userResponse.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Thanks for playing! Your final score is: " + score);
+        scanner.close();
+    }
+}
